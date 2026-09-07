500f93a90e304cccce18c08a572b491d80153619
iwasaki-hub <66316321+iwasaki-hub@users.noreply.github.com>
2026-09-08 00:49:27 +0900

    install packages in server side

--- server/server.js
+++ server/server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(cors());
+app.use(helmet());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello Express Server 👋" });
