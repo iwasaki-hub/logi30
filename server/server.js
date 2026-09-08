8f030989ea5780ea7997ffb546142c5ccd269111
iwasaki-hub <66316321+iwasaki-hub@users.noreply.github.com>
2026-09-08 21:50:30 +0900

    connectDB

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -5,11 +6,17 @@ const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
+const connectDB = require("./config/connectDB");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
 app.use(helmet());
 app.use(cookieParser());
 
@@ -22,6 +29,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello Express Server 👋" });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  await connectDB();
   console.log(`🚀 The server is running on port ${PORT}`);
 });
